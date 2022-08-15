82fd0bc556eebbb6f5320ce0935f0a8ac45303a3
sinitsynme <sinitsynme@outlook.com>
2022-08-15 15:55:04 +0400

    Add signatures to parsers

--- src/main/java/ru/sinitsynme/airportsearch/parser/TableParser.java
+++ src/main/java/ru/sinitsynme/airportsearch/parser/TableParser.java
@@ -1,4 +1,11 @@
 package ru.sinitsynme.airportsearch.parser;
 
+import java.util.List;
+import java.util.Map;
+
 public interface TableParser {
+
+    Map<Integer, String> getColumnIdAndData(int column);
+    String getRow(int id);
+
 }
--- src/main/java/ru/sinitsynme/airportsearch/parser/impl/CSVParser.java
+++ src/main/java/ru/sinitsynme/airportsearch/parser/impl/CSVParser.java
@@ -2,5 +2,17 @@ package ru.sinitsynme.airportsearch.parser.impl;
 
 import ru.sinitsynme.airportsearch.parser.TableParser;
 
+import java.util.Map;
+
 public class CSVParser implements TableParser {
+
+    @Override
+    public Map<Integer, String> getColumnIdAndData(int column) {
+        return null;
+    }
+
+    @Override
+    public String getRow(int id) {
+        return null;
+    }
 }
